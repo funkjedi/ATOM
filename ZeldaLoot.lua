5c39ffd7e7a613db94a4a1e23656c33fe64c49d3
Tim Robertson <funkjedi@gmail.com>
2022-12-03 14:09:37 -0500

    Adding ignore list to Zelda loot

--- ZeldaLoot.lua
+++ ZeldaLoot.lua
@@ -1,10 +1,20 @@
 local addonName, ATOM = ...
 local Module = ATOM:NewModule('ZeldaLoot')
 
-local colorQuality = {
+local colourQualitySounds = {
     [select(4, GetItemQualityColor(2))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\2.mp3',
     [select(4, GetItemQualityColor(3))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\3.mp3',
     [select(4, GetItemQualityColor(4))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\4.mp3',
+}
+
+local ignoredItems = {
+    ['Anima Gossamer'] = true,
+    ['Anima Webbing'] = true,
+    ['Genesis Mote'] = true,
+    ['Progenitor Essentia'] = true,
+    ['Protoflesh'] = true,
+    ['Sandworn Relic'] = true,
+    ['Silken Protofiber'] = true,
 }
 
 function Module:OnEnable()
@@ -16,10 +26,10 @@ function Module:OnDisable()
 end
 
 function Module:CHAT_MSG_LOOT(event, msg)
-    for color in msg:gmatch('ff[%da-f][%da-f][%da-f][%da-f][%da-f][%da-f]') do
-        if colorQuality[color] then
-            PlaySoundFile(colorQuality[color])
-            break
-        end
+    local name = string.match(msg, '|h%[([^%]]+)%]|h')
+    local colour = string.match(msg, '|c(%x+)|H')
+
+    if name and not ignoredItems[name] and colour and colourQualitySounds[colour] then
+        PlaySoundFile(colourQualitySounds[colour])
     end
 end
