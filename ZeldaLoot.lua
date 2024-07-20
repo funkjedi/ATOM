9cf5a6e53adda99e7660804d9735dc1793f582e3
Tim Robertson <funkjedi@gmail.com>
2024-07-20 03:54:26 -0400

    Add anti flood/spam support

--- ZeldaLoot.lua
+++ ZeldaLoot.lua
@@ -1,11 +1,12 @@
 local addonName, ATOM = ...
 local Module = ATOM:NewModule('ZeldaLoot')
 
-local colourQualitySounds = {
-    [select(4, GetItemQualityColor(2))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\2.mp3', -- Uncommon
-    [select(4, GetItemQualityColor(3))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\3.mp3', -- Rare
-    [select(4, GetItemQualityColor(4))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\4.mp3', -- Epic
-    [select(4, GetItemQualityColor(5))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\5.mp3', -- Legendary
+local colourQualities = {
+    [select(4, GetItemQualityColor(2))] = { sound = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\2.mp3', interval = 2 },
+    [select(4, GetItemQualityColor(3))] = { sound = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\3.mp3', interval = 2 },
+    [select(4, GetItemQualityColor(4))] = { sound = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\4.mp3', interval = 2 },
+    [select(4, GetItemQualityColor(5))] = { sound = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\5.mp3', interval = 7 },
+    [select(4, GetItemQualityColor(6))] = { sound = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\6.mp3', interval = 15 },
 }
 
 local ignoredItems = {
@@ -28,16 +29,32 @@ end
 
 function Module:CHAT_MSG_LOOT(event, msg, _, _, _, unitName)
     local item = string.match(msg, '|h%[([^%]]+)%]|h')
-    local colour = string.match(msg, '|c(%x+)|H')
 
-    local name, realm = UnitName('player')
-    local player = name .. '-' .. (realm or GetRealmName())
-
-    local validItem = name and not ignoredItems[item]
-    local validColour = colour and colourQualitySounds[colour]
-    local validPlayer = unitName == player or unitName == name
-
-    if validItem and validColour and validPlayer then
-        PlaySoundFile(colourQualitySounds[colour])
+    if not item or ignoredItems[item] then
+        return
     end
+
+    local colour = string.match(msg, '|c(%x+)|H')
+    local colourQuality = colour and colourQualities[colour]
+
+    if not colourQuality then
+        return
+    end
+
+    local player, realm = UnitName('player')
+    local playerID = player .. '-' .. (realm or GetRealmName())
+
+    if player ~= unitName and playerID ~= unitName then
+        return
+    end
+
+    -- avoid spamming the same sound
+    if (colourQuality.timestamp ~= nil) then
+        if (time() - colourQuality.timestamp < colourQuality.interval) then
+            return
+        end
+    end
+
+    colourQuality.timestamp = time()
+    PlaySoundFile(colourQuality.sound)
 end
