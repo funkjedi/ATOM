9f1ed9b358cb28dfe0e0c5a176396173e429871f
Tim Robertson <funkjedi@gmail.com>
2024-07-19 22:36:44 -0400

    Only alert for player loot

--- ZeldaLoot.lua
+++ ZeldaLoot.lua
@@ -2,9 +2,10 @@ local addonName, ATOM = ...
 local Module = ATOM:NewModule('ZeldaLoot')
 
 local colourQualitySounds = {
-    [select(4, GetItemQualityColor(2))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\2.mp3',
-    [select(4, GetItemQualityColor(3))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\3.mp3',
-    [select(4, GetItemQualityColor(4))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\4.mp3',
+    [select(4, GetItemQualityColor(2))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\2.mp3', -- Uncommon
+    [select(4, GetItemQualityColor(3))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\3.mp3', -- Rare
+    [select(4, GetItemQualityColor(4))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\4.mp3', -- Epic
+    [select(4, GetItemQualityColor(5))] = 'Interface\\AddOns\\ATOM\\Sounds\\ZeldaLoot\\5.mp3', -- Legendary
 }
 
 local ignoredItems = {
@@ -25,11 +26,18 @@ function Module:OnDisable()
     self:UnregisterEvent('CHAT_MSG_LOOT')
 end
 
-function Module:CHAT_MSG_LOOT(event, msg)
-    local name = string.match(msg, '|h%[([^%]]+)%]|h')
+function Module:CHAT_MSG_LOOT(event, msg, _, _, _, unitName)
+    local item = string.match(msg, '|h%[([^%]]+)%]|h')
     local colour = string.match(msg, '|c(%x+)|H')
 
-    if name and not ignoredItems[name] and colour and colourQualitySounds[colour] then
+    local name, realm = UnitName('player')
+    local player = name .. '-' .. (realm or GetRealmName())
+
+    local validItem = name and not ignoredItems[item]
+    local validColour = colour and colourQualitySounds[colour]
+    local validPlayer = unitName == player or unitName == name
+
+    if validItem and validColour and validPlayer then
         PlaySoundFile(colourQualitySounds[colour])
     end
 end
